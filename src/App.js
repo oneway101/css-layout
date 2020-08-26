0896e9f7b0562d6649ce144dc4f9210607e70632
Ha Na Gill <oneway.first@gmail.com>
2020-08-25 23:00:02 -0400

    Holy Grail with Flexbox

--- src/App.js
+++ src/App.js
@@ -4,23 +4,7 @@ import "./styles.css";
 export default function App() {
   return (
     <>
-      <header>
-        Header
-      </header>
-      <div className="container">
-        <aside className="left">
-          Left Sidebar
-        </aside>
-        <article className="content">
-          Article
-        </article>
-        <aside className="right">
-          Right Sidebar
-        </aside>
-      </div>
-      <footer>
-        Footer
-      </footer>
+      <button onClick="">Holy Grail with Flexbox</button>
     </>
   );
 }
---
+++ src/HolyGrailFlex.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+
+const HolyGrailFlex = () => {
+  return (
+    <>
+      <header>
+          Header
+        </header>
+      <div className="container">
+        <aside className="left">
+          Left Sidebar
+        </aside>
+        <article className="content">
+          Article
+        </article>
+        <aside className="right">
+          Right Sidebar
+        </aside>
+      </div>
+      <footer>
+        Footer
+      </footer>
+    </>
+  )
+}
+
+
+
+export default HolyGrailFlex
